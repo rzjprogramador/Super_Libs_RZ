6ac1ca96b68a0d55c9069c10e5eb2766897221a8
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-01-25 23:36:23 -0300

    feat: modules node and graphqltools

--- Typescript/SuperNode/modules/mod_path.ts
+++ Typescript/SuperNode/modules/mod_path.ts
@@ -7,3 +7,8 @@ import { join, dirname } from "https://deno.land/std@0.183.0/path/mod.ts"
 export {
   join, dirname
 }
+
+/*
+- fonte : medium >> https://medium.com/deno-the-complete-reference/handling-paths-in-deno-55f99de96f01
+-
+*/
